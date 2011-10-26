0375d8d314cc7f7763ac33e5abd0c5d1f449dea7
Kevin <kevin@ksturner.com>
2011-10-26 16:18:59 -0500

    new directory structure so that setup.py can point to real package name

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 
 from distutils.core import setup
 
-setup(name='pyfpdf',
+setup(name='PyFpdf',
       version='1.54c.1',
       description='Fork of PyFpdf project hosted on Google code',
       author='Mariano Reingart',
@@ -10,6 +10,6 @@ setup(name='pyfpdf',
       maintainer='Kevin Turner',
       maintainer_email='kevin@ksturner.com',
       url='http://code.google.com/p/pyfpdf/',
-      packages=[''],
-      package_data={'': ['invoice.csv']},
+      packages=['pyfpdf'],
+      package_data={'pyfpdf': ['invoice.csv']},
       )
