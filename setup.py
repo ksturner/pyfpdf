d1024d03235819dc7f141af13771533182f2b535
Kevin <kevin@ksturner.com>
2011-10-26 10:35:03 -0500

    first attempt at setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='PyFPdf',
+      version='1.54c.1',
+      description='Fork of PyFPdf project hosted on Google code',
+      author='Kevin Turner',
+      author_email='kevin@ksturner.com',
+      url='http://code.google.com/p/pyfpdf/',
+      packages=[''],
+      package_data={'': ['invoice.csv']},
+      )
